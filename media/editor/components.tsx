3d8bf1447e07c369ff95f98ff4107161d66e656c
AlexMorgan3817 <Ivan.zvereff@gmail.com>
2025-08-17 21:53:50 +0700

    Fixing dublicating of sprites during renaming, and fixing unreactive rename behaviour

--- media/editor/components.tsx
+++ media/editor/components.tsx
@@ -1,20 +1,26 @@
 import { VSCodeTextField } from "@vscode/webview-ui-toolkit/react";
 import React, { useEffect, useRef, useState } from "react";
+import { useGlobalHandler } from "./useHelpers";
 
 interface EditableFieldsProps extends React.HTMLProps<HTMLDivElement> {
     value: string;
     displayValue?: string;
     label?: string;
     onChange: (value: any) => void;
+    onEditingStateChanged?: (value: boolean) => void;
 }
 
 // Text editable by doubleclicking it
 export const EditableField: React.FC<EditableFieldsProps> = props => {
-    const { value, displayValue, label, onChange, ...rest } = props;
+    const { value, displayValue, label, onChange, onEditingStateChanged, ...rest } = props;
     const rawDisplayValue = displayValue || value;
     const [editing, setEditing] = useState(false);
     const inputRef = useRef(null);
-
+    useEffect(() => {
+        if (onEditingStateChanged) {
+            onEditingStateChanged(editing);
+        }
+    }, [editing, onEditingStateChanged]);
     const pushChange = () => {
         props.onChange((inputRef?.current as unknown as HTMLInputElement).value);
         setEditing(false);
@@ -23,15 +29,34 @@ export const EditableField: React.FC<EditableFieldsProps> = props => {
     const toggleEdit = () => {
         setEditing(!editing);
     };
-
+    const enableRename = () => {
+        setEditing(true);
+    };
     useEffect(() => {
         if (editing) {
             (inputRef?.current as unknown as HTMLInputElement).focus();
         }
     }, [editing]);
 
+    useGlobalHandler(
+        "keydown",
+        (e:KeyboardEvent) => {
+            if(editing){
+                switch (e.code) {
+                    case "Escape":
+                    case "Enter":
+                        setEditing(false);
+                        e.preventDefault();
+                        break;
+                }
+            }
+        },
+        [editing]
+    );
+
     return (
-        <div {...rest} onDoubleClick={toggleEdit}>
+        <div {...rest} onDoubleClick={editing ? undefined : toggleEdit}>
+            {!editing && rawDisplayValue}
             {editing && (
                 <VSCodeTextField
                     ref={inputRef}
@@ -40,7 +65,6 @@ export const EditableField: React.FC<EditableFieldsProps> = props => {
                     onBlur={() => setEditing(false)}
                 />
             )}
-            {!editing && rawDisplayValue}
         </div>
     );
 };
